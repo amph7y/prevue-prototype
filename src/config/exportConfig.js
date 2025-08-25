6f88fa42a980154aca7e4ad74d1807aa5eee17a7
MhmdAL <mohammadalnoss@outlook.com>
2025-08-25 18:16:49 +0300

    update batch size

--- src/config/exportConfig.js
+++ src/config/exportConfig.js
@@ -3,5 +3,5 @@ export const EXPORT_CAPS = {
     'pubmed': 500,    // Cap at 500 records
     'scopus': 250,    // Cap at 250 records  
     'embase': 250,    // Cap at 250 records 
-    'core': 5000      // Cap at 5000 records
+    'core': 2500      // Cap at 5000 records
 };
